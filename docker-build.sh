31982fcb8e1ac4f2294dbc73bcec1e1a6a145592
Tobias Koltsch <tobias@koltsch.de>
2020-11-06 08:56:18 +0100

    Add docker hub user when building Docker image

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker build . -t micronaut-application
+docker build . -t nerdcoding/micronaut-application
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run -p 8080:8080 micronaut-application"
+echo "    $ docker run -p 8080:8080 nerdcoding/micronaut-application"
